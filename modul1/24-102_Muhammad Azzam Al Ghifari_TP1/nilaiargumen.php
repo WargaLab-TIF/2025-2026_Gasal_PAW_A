bd8244cc8e43c5189facad8f686d8e921fd16a3e
azzamlghifari <azamalghifari06@gmail.com>
2025-09-30 15:22:22 +0000

    kirim tugas modul1

---
+++ modul1/24-102_Muhammad Azzam Al Ghifari_TP1/nilaiargumen.php
@@ -0,0 +1,10 @@
+<?php
+function setheight($mineheight = 50) {
+    echo "the height is : $mineheight <br>";
+}
+
+setheight(350);
+setheight();
+setheight(135);
+setheight(80);
+?>
---
+++ modul1/24-102_Muhammad Azzam Al Ghifari_TP1/variabel1.php
@@ -0,0 +1,4 @@
+<?php
+$txt = "w3school.com";
+echo "i love ".$txt;
+?>
