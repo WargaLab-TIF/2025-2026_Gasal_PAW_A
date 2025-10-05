041be2fb6b3cc6df90ae48d7d39829b85454bddb
IrmaAnnisatulJannah <bossnisaa@gmail.com>
2025-10-05 17:12:11 +0700

    mengumpulkan tm2

---
+++ modul2/24-014_Irma Annisatul Jannah/soal9.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Grade Nilai Mahasiswa</title>
+</head>
+<body>
+    
+    <h2>Cek Grade Nilai Mahasiswa</h2>
+
+    <form method="post">
+        Masukkan Nilai: 
+        <input type="number" name="nilai" required>
+        <input type="submit" value="Cek Grade">
+    </form>
+
+    <?php
+    if (isset($_POST['nilai'])) {
+        $nilai = $_POST['nilai'];
+
+        if ($nilai >= 85) {
+            $grade = "A";
+        } elseif ($nilai >= 70) {
+            $grade = "B";
+        } elseif ($nilai >= 55) {
+            $grade = "C";
+        } elseif ($nilai >= 40) {
+            $grade = "D";
+        } else {
+            $grade = "E";
+        }
+        echo "<br>Nilai: $nilai <br>";
+        echo "Grade: $grade";
+    }
+    ?>
+</body>
+</html>
