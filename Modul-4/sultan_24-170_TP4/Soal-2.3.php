1140aee1cefef2f4140f44e8c7f2c5f4a6b3943c
Pet-J55 <kartunbahasaindonesia1@gmail.com>
2025-10-26 22:36:18 +0700

    Add files via upload

---
+++ Modul-4/sultan_24-170_TP4/Soal-2.3.php
@@ -0,0 +1,47 @@
+<legend>Person details</legend>
+<form name="myForm" action="processData_form.php" method="post">
+<div class="field">
+            <label for="surname">Surname</label>
+            <input type="text" name="surname" id="surname">
+        </div>
+        <div class="field">
+            <label for="email">email address</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div class="field">
+            <label for="password">password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div class="field">
+            <label for="address">Street address</label>
+            <textarea  name="address" id="address" rows="3" cols="22"></textarea>
+        </div>
+        <div class="field">
+            <label for="state">State</label>
+            <select name="state" id="state">
+                <option value="1">Australian Capital Teritory</option>
+                <option value="2">Queensland</option>
+                <option value="3">Victoria</option>
+                <option value="4">New south wales</option>
+                <option value="5">Tazmania</option>
+                <option value="6">South australia</option>
+                <option value="7">Western australia</option>
+                <option value="8">Northern australia</option>
+            </select>
+        </div>
+        <div class="field">
+            <input type="hidden" name="country" value="Australia">
+        </div>   
+        <div class="field">
+            <label for="gender">gender</label>
+            <input type="radio" name="gender" value="male">male
+            <input type="radio" name="gender" value="female">female
+        </div>   
+        <div class="field">
+            <label for="vegetarian">Vegetarian</label>
+            <input type="checkbox" name="vegetarian">
+        </div>   
+        <div class="field">
+            <input type="submit" name="submit" value="Submit"><input type="reset" value="Reset" name="reset">
+        </div>
+    </form>
