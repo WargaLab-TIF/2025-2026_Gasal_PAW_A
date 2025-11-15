d12eb17b86f3be82392e4e890253c6fdcefcdc24
Roti18 <akuronif@gmail.com>
2025-11-15 19:33:39 +0700

    submit: module7

---
+++ modul7/24-085_Moch._Zamroni_Fahreza/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Input Date</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+  <form action="./chart.php" method="post">
+    <input type="date" name="dari" id="dari">
+    <input type="date" name="sampai" id="sampai">
+    <button name="tampilkan">Tampilkan</button>
+  </form>
+</body>
+
+</html>
